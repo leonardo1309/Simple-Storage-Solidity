b231e1e6372d09fac84538d6f2a80ef7e8c4673f
Leonardo <leonardo.mu00@gmail.com>
2022-02-14 08:05:49 -0500

    Brownie simple_storage finish

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
 
@@ -14,6 +14,13 @@ def deploy_simple_storage():
     # account = accounts.load("test-account")
     # account = accounts.add(os.getenv("PRIVATE_KEY"))
     # account = accounts.add(config["wallets"]["from_key"])
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
